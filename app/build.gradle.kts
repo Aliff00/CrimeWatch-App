af53461cd95c2145cecab0c4840a2855afa52df3
fasyyuu <fasyy007@gmail.com>
2023-12-27 11:18:33 +0800

    Main page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.crimewatch"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.crimewatch"
@@ -36,7 +36,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
+    implementation("androidx.navigation:navigation-ui:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
 }
--- app/src/main/java/com/example/crimewatch/MainActivity.java
+++ app/src/main/java/com/example/crimewatch/MainActivity.java
@@ -1,14 +1,99 @@
 package com.example.crimewatch;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ReportFragment reportFragment = new ReportFragment();
+                reportFragment.show(getSupportFragmentManager(), reportFragment.getTag());
+            }
+        });
+
+        // bind NavHostFragment with NavController ; NavController manage app nav in NavHost
+        NavHostFragment host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.NHFMain);
+        NavController navController = host.getNavController();
+        setupBottomNavMenu(navController);
+
+        // sending notification alert (alertdialog)
+        // check nearby crimes - needs report and maps
+        boolean crimesNearby = true;
+
+        if(crimesNearby){
+            showGreenZone();
+        } else{
+            showRedZone();
+        }
+    }
+
+    // set up bottomnav with navcontroller
+    private void setupBottomNavMenu(NavController navController){
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_view);
+        NavigationUI.setupWithNavController(bottomNav,navController);
+    }
+
+    //notification alert method
+    private void showGreenZone() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("You are in Green Zone.");
+        builder.setMessage("0 crimes reported within 1km radius. Stay Safe.");
+        builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss(); // go to map
+            }
+        });
+        builder.show();
+    }
+    private void showRedZone() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("You are in Red Zone.");
+        builder.setMessage("20 crimes reported within 1km radius. Be Careful.");
+        builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getApplicationContext(), MapsFragment.class);
+                startActivity(intent);
+            }
+        });
+        builder.show();
     }
 }
