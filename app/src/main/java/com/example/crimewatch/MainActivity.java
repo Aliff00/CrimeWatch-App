b444ceb3ee5d0a978183aee5957ae9452bafb72b
fasyyuu <fasyy007@gmail.com>
2024-01-08 04:51:21 +0800

    Update Notification

--- app/src/main/java/com/example/crimewatch/MainActivity.java
+++ app/src/main/java/com/example/crimewatch/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.crimewatch;
 
 import android.app.Dialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -14,9 +20,13 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -25,19 +35,54 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
-        FloatingActionButton fab;
+    FloatingActionButton fab;
     DrawerLayout drawerLayout;
     BottomNavigationView bottomNavigationView;
-    private static final String TAG = "FirestoreListener";
-    private FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String currentUserId = currentUser.getUid();
+
+        createNotificationChannel();
+
+        CollectionReference reportRef = db.collection("report");
+
+        reportRef.whereEqualTo("user",currentUserId).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    return;
+                }
+                if (value != null && !value.isEmpty()) {
+                    for (QueryDocumentSnapshot document : value) {
+                        // Check if the "status" field has changed
+                        String currentStatus = document.getString("status");
+                        String previousStatus = getPreviousStatus(getApplicationContext(), document.getId()); // Implement this method
+                        if (previousStatus == null || !previousStatus.equals(currentStatus)) {
+                            // Status has changed, show notification
+                            showNotification(currentStatus);
+                            // Update the previous status
+                            setPreviousStatus(getApplicationContext(), document.getId(), currentStatus); // Implement this method
+                        }
+                    }
+                }
+            }
+        });
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         fab = findViewById(R.id.fab);
@@ -46,12 +91,12 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences preferences = getSharedPreferences("myPrefs", MODE_PRIVATE);
         boolean isLoggedIn = FirebaseAuth.getInstance().getCurrentUser() != null;
         boolean rememberMeChecked = preferences.getBoolean("rememberMe", false);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open_nav,R.string.close_nav);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_nav, R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        if(savedInstanceState==null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,new HomeFragment()).commit();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, new HomeFragment()).commit();
 
         }
         replaceFragment(new HomeFragment());
@@ -60,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnItemSelectedListener(item -> {
 
             int id = item.getItemId();
-            if(id == R.id.Home){
+            if (id == R.id.Home) {
                 replaceFragment(new HomeFragment());
             } else if (id == R.id.Maps) {
                 Intent intent = new Intent(getApplicationContext(), MapFragment.class);
@@ -82,82 +127,80 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent); // Start the ShortsActivity
             }
         });
-//        NavHostFragment host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.NHFMain);
-//        NavController navController = host.getNavController();
-//        NavigationUI.setupWithNavController(bottomNavigationView,navController);
-//
-//        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
-//            @Override
-//            public void onDestinationChanged(@NonNull NavController controller,
-//                                             @NonNull NavDestination destination, @Nullable Bundle arguments) {
-//                if(destination.getId() == R.id.SupportEmergency) {
-//                    bottomNavigationView.setVisibility(View.GONE);
-//                    bottomNavigationView.setVisibility(View.GONE);
-//                    fab.setVisibility(View.GONE);
-//                } else {
-//                    bottomNavigationView.setVisibility(View.VISIBLE);
-//                    bottomNavigationView.setVisibility(View.VISIBLE);
-//                    fab.setVisibility(View.VISIBLE);
-//                }
-//            }
-//        });
-//
-//        db = FirebaseFirestore.getInstance();
-//
-//        CollectionReference collectionReference = db.collection("report");
-//
-//        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-//                if(error!=null){
-//                    Log.w(TAG, "Listen failed.", error);
-//                    return;
-//                }
-//
-//                if (value != null && !value.isEmpty()) {
-//                    for (DocumentChange dc : value.getDocumentChanges()) {
-//                        if (dc.getType() == DocumentChange.Type.ADDED) {
-//                            // Logic to handle the new document added
-//                            Object messageData = dc.getDocument().getData().get("messageField"); // Replace with the actual field name
-//                            String messageString;
-//
-//                            if (messageData instanceof String) {
-//                                messageString = (String) messageData;
-//                            } else if (messageData instanceof Number) {
-//                                messageString = String.valueOf(messageData);
-//                            } else {
-//                                // Handle other data types or provide a fallback message
-//                                messageString = "Unknown message data type";
-//                            }
-//                            // You can perform actions like showing an alert dialog here
-//                            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-//                            builder.setCancelable(true);
-//                            builder.setMessage(messageString);
-//                            builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
-//                                @Override
-//                                public void onClick(DialogInterface dialog, int which) {
-//                                    dialog.cancel();
-//                                }
-//                            });
-//                            builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
-//                                @Override
-//                                public void onClick(DialogInterface dialog, int which) {
-//                                    dialog.dismiss();
-//                                    // go to maps
-//                                }
-//                            });
-//                            if (!isDestroyed() && !isFinishing()) {
-//                                builder.show(); // Show dialog only if activity is not destroyed or finishing
-//                            }
-//                        }
-//                    }
-//                } else {
-//                    System.out.println("No new documents in the collection.");
-//                }
-//            }
-//        });
-
     }
+
+    private static final String CHANNEL_ID = "CrimeWatch"; // Replace with your desired ID
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Crime";
+            String description = "Report status update";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            // Create a notification channel with the given ID, name, and importance
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            // Register the channel with the system
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private static final String PREF_NAME = "ReportStatusPrefs";
+
+    // Get the previous status for a specific report ID
+    public static String getPreviousStatus(Context context, String reportId) {
+        SharedPreferences prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        return prefs.getString(reportId, null);
+    }
+
+    // Set the previous status for a specific report ID
+    public static void setPreviousStatus(Context context, String reportId, String status) {
+        SharedPreferences prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(reportId, status);
+        editor.apply();
+    }
+
+    // Clear the previous status for a specific report ID
+    public static void clearPreviousStatus(Context context, String reportId) {
+        SharedPreferences prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.remove(reportId);
+        editor.apply();
+    }
+    private void showNotification(String current) {
+
+        // Create an explicit intent for an Activity in your app.
+        Intent intent = new Intent(this, historyArchive.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.emojione_bw_1f989)
+                .setContentTitle("Report "+current)
+                .setContentText("The status of your report has been updated.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        // Add as notification
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);//(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        manager.notify(0, builder.build());
+    }
+
     private  void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -225,26 +268,4 @@ public class MainActivity extends AppCompatActivity {
     public SharedPreferences getMyPreferences() {
         return getSharedPreferences("myPrefs", MODE_PRIVATE);
     }
-
-//    private void showGreenZone() {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//        builder.setCancelable(true);
-//        builder.setTitle("Watch out ! New crimes reported.");
-//        builder.setMessage("Location : Bangsar, KL"); // get location?
-//        builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.cancel();
-//            }
-//        });
-//        builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.dismiss();
-//                // go to maps
-//            }
-//        });
-//        builder.show();
-//    }
-
 }
